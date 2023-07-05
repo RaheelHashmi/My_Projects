6fbbac64b5b484be4dbd9e622d464e53d9ba5d3c
Raheel Hashmi <89994946+RaheelHashmi@users.noreply.github.com>
2023-07-06 03:32:42 +0500

    Add files via upload

--- CS50P_mini_projects/Problem_Set_2/Just_setting_up_my_twttr/twttr.py
+++ CS50P_mini_projects/Problem_Set_2/Just_setting_up_my_twttr/twttr.py
@@ -5,9 +5,19 @@ In a file called twttr.py, implement a program that prompts the user for a str
 of text and then outputs that same text but with all vowels (A, E, I, O, and U) 
 omitted, whether inputted in uppercase or lowercase.
 
-Twitter ---------------> Twttr
-What's your name? -----> Wht's yr nm?
-CS50 ------------------> CS50
+Here’s how to test your code manually:
+
+Run your program with python twttr.py. Type Twitter and press Enter. Your 
+program should output:
+Twttr   
+
+Run your program with python twttr.py. Type What's your name? and press Enter. 
+Your program should output:
+Wht's yr nm?
+
+Run your program with python twttr.py. Type CS50 and press Enter. Your program 
+should output
+CS50
 """
 user_input = input("Input: ")
 vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
