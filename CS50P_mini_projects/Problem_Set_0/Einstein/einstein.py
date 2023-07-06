da0895177b0551add1f3c8b84a5b149f075305e8
Raheel Hashmi <89994946+RaheelHashmi@users.noreply.github.com>
2023-07-07 03:06:23 +0500

    Add files via upload

--- CS50P_mini_projects/Problem_Set_0/Einstein/einstein.py
+++ CS50P_mini_projects/Problem_Set_0/Einstein/einstein.py
@@ -9,9 +9,19 @@ In a file called einstein.py, implement a program in Python that prompts the
 user for mass as an integer (in kilograms) and then outputs the equivalent 
 number of Joules as an integer. Assume that the user will input an integer.
 
-1 ---------> 90000000000000000
-14 --------> 1260000000000000000
-50 --------> 4500000000000000000
+Here’s how to test your code manually:
+
+Run your program with python einstein.py. Type 1 and press Enter. Your program 
+should output:
+90000000000000000
+
+Run your program with python einstein.py. Type 14 and press Enter. Your program 
+should output:
+1260000000000000000
+
+Run your program with python einstein.py. Type 50 and press Enter. Your program 
+should output
+4500000000000000000
 """
 
 
