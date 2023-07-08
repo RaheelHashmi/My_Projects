c121a142487a4dd7f41d09d43b5daa65fc316141
Raheel Hashmi <89994946+RaheelHashmi@users.noreply.github.com>
2023-07-08 23:56:52 +0500

    Add files via upload

--- CS50P_mini_projects/Problem_Set_0/Indoor_Voice/indoor.py
+++ CS50P_mini_projects/Problem_Set_0/Indoor_Voice/indoor.py
@@ -9,8 +9,16 @@ whitespace should be outputted unchanged. You’re welcome, but not required, to
 prompt the user explicitly, as by passing a str of your own as an argument to 
 input.
 
-HELLO ----------------> hello
-THIS IS CS50 ---------> this is cs50
-50 -------------------> 50
+Here’s how to test your code manually. At the indoor/ $ prompt in your terminal:
+
+Run your program with python indoor.py. Type HELLO and press Enter. Your program 
+should output hello.
+Run your program with python indoor.py. Type THIS IS CS50 and press Enter. Your 
+program should output this is cs50.
+Run your program with python indoor.py. Type 50 and press Enter. Your program 
+should output 50.
+If you run into an error saying your file cannot be opened, retrace your steps 
+to be sure that you are inside your indoor folder and have saved your indoor.py 
+file there. Remember how?
 """
 print(input("Say something: ").lower())
